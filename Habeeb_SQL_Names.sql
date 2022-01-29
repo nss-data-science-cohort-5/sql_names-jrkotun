ae8285aa96b8e31d992e034983d91b7207bc8d8d
jrkotun <hjrkotun@gmail.com>
2022-01-29 13:59:14 -0600

    Still need to finish questions 17 and 18

--- Habeeb_SQL_Names.sql
+++ Habeeb_SQL_Names.sql
@@ -17,7 +17,8 @@ FROM names;
 SELECT name, year, MAX(num_registered) AS max_registered
 FROM names
 GROUP BY name, year
-ORDER BY max_registered DESC;
+ORDER BY max_registered DESC
+LIMIT 5;
 -- Answer: Linda had the most appearances in a year with 99689 appearances in 1947.
 
 -- Question 4
@@ -29,7 +30,8 @@ FROM names;
 SELECT year, SUM(num_registered) AS total_registered
 FROM names
 GROUP BY year
-ORDER BY total_registered DESC;
+ORDER BY total_registered DESC
+LIMIT 1;
 -- Answer: 1957 had the largest number of registrations at 4200022 registrations.
 
 -- Question 6
@@ -38,10 +40,10 @@ FROM names;
 -- Answer: There are 98400 distinct names in the dataset.
 
 -- Question 7
-SELECT gender, COUNT(gender) AS gender_count
+SELECT gender, SUM(num_registered) AS total_count
 FROM names
 GROUP BY gender;
--- Answer: There are more females registered.
+-- Answer: There are more males registered.
 
 -- Question 8
 SELECT name, gender, SUM(num_registered) AS total_registered
@@ -79,5 +81,58 @@ ORDER BY total_registered DESC;
 SELECT COUNT(DISTINCT name) AS distinct_count
 FROM names
 WHERE name LIKE ('Q%')
-AND name NOT LIKE ('_u');
--- Answer: There are 537 distinct names that start with a 'Q', but whose second letter is not 'u'.
+AND name NOT LIKE ('_u%');
+-- Answer: There are 46 distinct names that start with a 'Q', but whose second letter is not 'u'.
+
+-- Question 13
+SELECT name, SUM(num_registered) AS total_registered
+FROM names
+WHERE name IN ('Stephen', 'Steven')
+GROUP BY name;
+-- Answer: Steven is the more popular spelling between "Stephen" and "Steven".
+
+-- Question 14
+SELECT name, COUNT(DISTINCT gender)
+FROM names
+GROUP BY name
+HAVING COUNT(DISTINCT gender) = 2;
+
+SELECT (10773.0 / 98400) * 100
+-- Answer: 10.95% of names are "unisex".
+
+-- Question 15
+SELECT name, COUNT (DISTINCT year)
+FROM names
+GROUP BY name
+HAVING COUNT (DISTINCT year) = 139;
+-- Answer: 921 names have made an appearance in every single year since 1880.
+
+-- Question 16
+SELECT name, COUNT (DISTINCT year)
+FROM names
+GROUP BY name
+HAVING COUNT (DISTINCT year) = 1;
+-- Answer: 21123 names have only appeared in one year.
+
+-- Question 17
+SELECT DISTINCT(name), year
+FROM names
+WHERE year NOT BETWEEN 1880 AND 1949
+AND year NOT BETWEEN 1960 AND 2018
+ORDER BY name;
+-- Answer:  names only appeared in the 1950s.
+
+-- Question 18
+SELECT COUNT (DISTINCT name) as names_1950s
+FROM names
+WHERE year between 1950 and 1959
+  and name not in
+      (SELECT DISTINCT name FROM names WHERE year < 1950 or year > 1959);
+-- Answer:
+
+-- Question 19
+SELECT name, 2018 - MAX(year) AS difference
+FROM names
+GROUP BY name
+ORDER BY difference DESC;
+-- Answer: Roll and Zilpah are the names that have not been used in the longest.
